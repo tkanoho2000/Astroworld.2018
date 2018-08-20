21fd727d7be43364bdfc2a6bb959a76561b85e8c
APCS2nd <APCS2nd@MHS-Dreyer-11>
2018-08-20 09:45:56 -0700

    First project to commit to GitHub for 2018

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,15 @@
+
+/**
+ * Print out a message saying Hello!
+ * 
+ * @author Tyler Kanoho
+ * @version 8/17/2018
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Tyler Kanoho");//Display the string
+    }
+}
+
