12b72bceb2bb218ad0e34693ac5a6767c6d5e603
Tyler <Tyler@10.0.1.197>
2018-11-18 20:16:06 -0800

    haha

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -10,6 +10,7 @@ public class Hello_world
     public static void main(String[] args)
     {
         System.out.println("Hello World! From Tyler Kanoho");//Display the string
+        System.out.println("emi is amazing");
     }
 }
 
